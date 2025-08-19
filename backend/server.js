8786d40340b09d805f3a15f4f21873cd1946376b
Jamesz4147 <udomsap.ro.65@ubu.ac.th>
2025-08-19 19:33:40 +0700

    Change DB to Postgresql

--- backend/server.js
+++ backend/server.js
@@ -543,6 +543,6 @@ app.use((err, req, res, next) => {
 });
 
 // ✅ **Server Start**
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
 });
